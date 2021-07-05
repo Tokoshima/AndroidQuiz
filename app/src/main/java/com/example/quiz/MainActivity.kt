4c3b1f807cb586e6eefd482a9d6b1d95f657cbc2
Tokoshima <louwster3000@gmail.com>
2021-07-05 15:18:43 +0200

    Added toast

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import java.time.ZoneOffset
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton : Button
@@ -16,8 +19,22 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
         trueButton.setOnClickListener{ view: View ->
 
+            val toast = Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT)
+                toast.setGravity(Gravity.TOP,0,0)
+                toast.show()
+
+
         }
         falseButton.setOnClickListener { view: View ->
+            val toast = Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT)
+                toast.setGravity(Gravity.TOP,0,0)
+                toast.show()
 
         }
     }
